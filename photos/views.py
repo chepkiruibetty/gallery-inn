fe2dfbc6290b9b71f7d7dea069462b3b251734eb
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-02-28 16:50:17 +0300

    created navbar template

--- photos/views.py
+++ photos/views.py
@@ -4,6 +4,22 @@ from django.views.generic import ListView
 from .models import Images,Category,Location
 
 # Create your views here.
-class PhotosListView(ListView):
-    model = Images
-    template_name ='home.html'
+def home(request):
+    images= Images.objects.all()
+    return render(request, "home.html", {"images":images})
+
+def search_image(request):
+    categories=Category.objects.all()
+    locations=Location.objects.all()
+    if 'images' in request.GET and request.GET['image']:
+        search_item =request.GET.get('image')
+        found_results=Image.get_image_by_cate(search_item)
+        message=f"{search_item}"
+        print(found_results)
+        
+        return render(request, 'search.html',
+                    {'found_results': found_results, 'message': message, 'categories': categories,
+                    "locations": locations})
+    else:
+        message = 'You havent searched yet'
+        return render(request, 'search.html', {"message": message})
