519384d8f008ff8bbe246c5aa766adc90fd8839b
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-02-28 20:34:04 +0300

    corrected the synthax error

--- photos/urls.py
+++ photos/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns=[
     path('',views.home,name='home'),
-    path('',views.search_image,name='search_image')
+    path('',views.search_image,name='search_image'),
+    path('',views.categories,name='categories')
     
 ]
