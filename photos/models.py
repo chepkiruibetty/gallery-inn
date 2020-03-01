5e8f2d8af81ea3664b049b318df9b283a950b76c
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-03-01 10:03:31 +0300

    created method for search image by category

--- photos/models.py
+++ photos/models.py
@@ -3,9 +3,17 @@ from django.db import models
 # Create your models here.
 class Images(models.Model):
     name = models.CharField(max_length = 60)
+    image=models.ImageField(upload_to ='images/')
     description = models.TextField()
     image_location = models.ForeignKey('Location', on_delete=models.CASCADE,)
     image_category = models.ForeignKey('Category', on_delete=models.CASCADE,)
+    
+    
+    @classmethod
+    def search_image(cls,category):
+        images = cls.objects.filter(image_category__name =category )
+        
+        return images
     
     @classmethod 
     def get_all_images(cls):
@@ -22,22 +30,19 @@ class Images(models.Model):
     
     @classmethod
     def update_image(cls, id, value):
-        cls.objects.filter(id=id).update(value)
+        cls.objects.filter(id=id).update(image.value)
 
     @classmethod
-    def search_image_by_id(cls,id):
+    def get_image_by_id(cls,id):
         image = cls.objects.filter(id= id).all()
         return image
 
+    
     @classmethod
-    def search_image_by_cate(cls,cate):
-        images = cls.objects.filter(image_category__cate__contains=cate)
-        return images
+    def filter_by_location(cls,location):
+        images_location = cls.objects.filter(image_location_name=location)
+        return images_location
 
-    @classmethod
-    def search_image_by_location(cls,locs):
-        images = cls.objects.filter(image_location__locs__contains=locs).all()
-        return images
     
     def __str__(self):
         return self.name
@@ -53,6 +58,9 @@ class Location(models.Model):
     )
     locs = models.CharField(max_length = 255, choices = locations)
     
+    class Meta:
+        verbose_name_plural = 'Location'
+    
     @classmethod
     def save_location(self):
         self.save()
@@ -64,10 +72,6 @@ class Location(models.Model):
     @classmethod
     def update_locs(cls, id, new_locs):
         cls.objects.filter(id=id).update(locs=new_locs)
-
-        
-        
-    
     
     def __str__(self):
         return f"{self.locs}"
@@ -82,6 +86,9 @@ class Category(models.Model):
         ('People','People')
     )
     cate = models.CharField(max_length = 255, choices = categories)
+    class Meta:
+        verbose_name_plural = 'categories'
+    
     
     @classmethod
     def save_category(self):
@@ -93,7 +100,8 @@ class Category(models.Model):
         
     @classmethod
     def update_cate(cls, id, new_cate):
-        cls.objects.filter(id=id).update(cat=new_cate)
+        cls.objects.filter(id=id).update(cate=new_cate)
+        
     
     def __str__(self):
         return f"{self.cate}"
