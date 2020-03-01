4911841454cb2d8d1aafef09a09c0048a8909328
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-03-01 10:02:36 +0300

    created url for search location

--- photos/urls.py
+++ photos/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
+from .views import SearchResultsListView
 
 urlpatterns=[
     path('',views.home,name='home'),
-    path('',views.search_image,name='search_image'),
-    path('',views.categories,name='categories')
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
+    path('',views.search_location,name='search_location'),
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
